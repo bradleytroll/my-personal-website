d5cfcf8f70d4de7aec2120a6f5dabd0dadfdfb40
Bradley <bradleytroll@gmail.com>
2023-04-07 19:49:08 -0400

    Added additional click event

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
 
+// Wait for the DOM to load before adding the event listener
+document.addEventListener("DOMContentLoaded", function() {
+  
+    // Get the button element
+    const myButton = document.getElementById("myButton");
+    
+    // Add an event listener to the button
+    myButton.addEventListener("click", function() {
+      // Replace the text of the button with "Clicked!"
+      myButton.textContent = "You're awesome!";
+    });
+    
+  });
+  
